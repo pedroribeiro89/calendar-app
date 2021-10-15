8a0d03204bc3eabce20b98d8f678c3f69681dd42
pedroribeiro89 <pedroribeiro89@hotmail.com>
2021-10-14 22:57:33 -0300

    create project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: 'events', loadChildren: () => import('./pages/events/events.module').then((m) => m.EventsModule), },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
