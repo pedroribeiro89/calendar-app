8d5390be3295b496c1bdbe4611904f808b0ef589
pedroribeiro89 <pedroribeiro89@hotmail.com>
2021-10-17 15:44:51 -0300

    Calendar events

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,8 +1,20 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {FormControl} from "@angular/forms";
 
 export interface ICalendarParams {
   yearOptions: number[];
+  events: ICalendarEvent[];
+}
+
+export interface ICalendarEvent {
+  date: Date;
+  name: string;
+  data: any;
+}
+
+export interface ICalendarDay {
+  day: Date;
+  events: ICalendarEvent[];
 }
 
 @Component({
@@ -15,28 +27,36 @@ export class CalendarComponent implements OnInit {
   yearOptions: number[] = [];
   yearControl = new FormControl(null);
   public selectedMonth = 0;
-  public daysArray: Date[] = [];
+
+  private events: ICalendarEvent[] = [];
+  public daysArray: ICalendarDay[] = [];
 
   @Input() set calendarParams(params: ICalendarParams)  {
     if (params.yearOptions.length > 0) {
       this.yearOptions = params.yearOptions;
       this.yearControl.setValue(this.yearOptions[0]);
+      this.events = params.events;
       this.buildDays();
     }
   }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  private isSameDay(date1: Date, date2: Date) {
+    return date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate();
   }
 
   private buildDays() {
-
     const monthDaysNum = new Date(this.yearControl.value, this.selectedMonth+1, 0).getDate();
-    console.log(this.yearControl.value, this.selectedMonth, monthDaysNum);
     this.daysArray = [];
     for (let i = 0; i < monthDaysNum; ++i) {
-      this.daysArray.push(new Date(this.yearControl.value, this.selectedMonth, i+1));
+      const day = new Date(this.yearControl.value, this.selectedMonth, i+1);
+      const events = this.events.filter((event: ICalendarEvent) => this.isSameDay(day, event.date));
+      this.daysArray.push({day, events});
     }
   }
 
@@ -47,5 +67,6 @@ export class CalendarComponent implements OnInit {
     this.selectedMonth = month;
     this.buildDays();
   }
+
 
 }
--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -8,13 +8,36 @@ import {ICalendarParams} from "../../components/calendar/calendar.component";
 })
 export class EventsComponent implements OnInit {
 
-  calendar: ICalendarParams = {yearOptions: []};
+  calendar: ICalendarParams = {yearOptions: [], events: []};
 
   constructor() { }
 
   ngOnInit(): void {
     this.calendar = {
-      yearOptions: [2020, 2019]
+      yearOptions: [2020, 2019],
+      events: [
+        {
+          date: new Date("2019-08-09"),
+          name: "Liverpool FC x Norwich City FC",
+          data: {x:'data'}
+        },
+        {
+          date: new Date("2019-08-10"),
+          name: "West Ham United FC x Manchester City FC",
+          data: {x:'data'}
+        },
+        {
+          date: new Date("2019-08-17"),
+          name: "Burnley FC x Southampton FC",
+          data: {x:'data'}
+        }
+     ]
     };
+    const x = new Date("2019-08-17");
+    console.log('x', x);
+    console.log('x', new Date(x.getTime() + x.getTimezoneOffset() * 60000));
+    console.log('x', x.getFullYear(),x.getMonth(), x.getDay());
+
   }
+
 }
