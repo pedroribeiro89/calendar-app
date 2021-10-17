96b38a8ccf708d36b49af44ebe2d24bd9258e36b
pedroribeiro89 <pedroribeiro89@hotmail.com>
2021-10-17 11:51:56 -0300

    calendar structure and design

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -14,12 +14,14 @@ export class CalendarComponent implements OnInit {
 
   yearOptions: number[] = [];
   yearControl = new FormControl(null);
-  public selectedMonth = 1;
+  public selectedMonth = 0;
+  public daysArray: Date[] = [];
 
   @Input() set calendarParams(params: ICalendarParams)  {
     if (params.yearOptions.length > 0) {
       this.yearOptions = params.yearOptions;
       this.yearControl.setValue(this.yearOptions[0]);
+      this.buildDays();
     }
   }
 
@@ -28,8 +30,22 @@ export class CalendarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private buildDays() {
+
+    const monthDaysNum = new Date(this.yearControl.value, this.selectedMonth+1, 0).getDate();
+    console.log(this.yearControl.value, this.selectedMonth, monthDaysNum);
+    this.daysArray = [];
+    for (let i = 0; i < monthDaysNum; ++i) {
+      this.daysArray.push(new Date(this.yearControl.value, this.selectedMonth, i+1));
+    }
+  }
+
+  onYearChange() {
+    this.buildDays();
+  }
   onMonthChange(month: number) {
     this.selectedMonth = month;
+    this.buildDays();
   }
 
 }
